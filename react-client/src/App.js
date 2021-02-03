0a826abe5d996b6717d0e94c6561df10759f01bc
Eunice Pereira <eunicepere333@gmail.com>
2021-02-03 09:57:19 -0600

    fixed router

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import Home from './UI/Home';
+
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+import axios from 'axios';
+
+const App = () => {
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		axios.get('/api').then((response) => {
+			console.log(response.data);
+			setData(response.data);
+		});
+	}, []);
+
 	return (
-		<div className="App">
-			<h1>Soul Reads</h1>
-		</div>
+		<Router>
+			<div className="App">
+				<Home data={data} />
+			</div>
+		</Router>
 	);
-}
+};
 
 export default App;
