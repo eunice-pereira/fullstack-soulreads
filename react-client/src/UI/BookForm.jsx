0a4b528e7e017b178d0a04d7030816be28628ccb
Eunice Pereira <eunicepere333@gmail.com>
2021-02-08 08:51:28 -0600

    modified add book function

--- react-client/src/UI/BookForm.jsx
+++ react-client/src/UI/BookForm.jsx
@@ -1,70 +1,115 @@
-import React, {  useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 function BookForm(props) {
-    const [title, setTitle] = useState("");
-    const [returned, setReturned] = useState([]);
+	const [title, setTitle] = useState('');
+	const [returned, setReturned] = useState([]);
+	const [bookTitle, setBookTitle] = useState('');
+	const [bookAuthor, setBookAuthor] = useState([]);
+	const [bookCategory, setBookCategory] = useState([]);
+	const [bookDesc, setBookDesc] = useState('');
+	const [bookImage, setBookImage] = useState('');
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        search(title)
-        alert(`Searching Title ${title}`)
-    }
+	// const handleSubmit = (evt) => {
+	// 	evt.preventDefault();
+	// 	search(title);
+	// };
 
+	const search = (query) => {
+		const url = `https://www.googleapis.com/books/v1/volumes?q=search+terms=${query}`;
 
-function addBook () {
-  async function retrieveBook() {
-    const resp = await axios.post('/api/book.js');
-    console.log(resp);
-  };
-};
-// const postBook = returned.map((book,i) => {
-  
-//   });
+		fetch(url)
+			.then((results) => results.json())
+			.then((data) => {
+				setReturned(data.items);
+			});
+	};
 
-  const search = query => {
-    const url = `https://www.googleapis.com/books/v1/volumes?q=search+terms=${query}`;
-   
+	// useEffect((e) => {
+	// 	search();
+	// }, []);
+	console.log(returned);
 
-    fetch(url)
-      .then(results => results.json())
-      .then(data => {
-      
-        setReturned(data.items)
-      });
-  };
-  console.log(returned)
-    return (
-      <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Book Title:
-          <input
-            type="text"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Search" />
-      </form>
-      {returned.length&&returned.map((item) => (
-        <div>{item.volumeInfo.title}
-        <p>{item.volumeInfo.authors}</p>
-        <p>{item.volumeInfo.categories}</p>
-        <p>{item.volumeInfo.description}</p>
-        <img src={item.volumeInfo.imageLinks.thumbnail} alt="Book Cover"></img> 
-        <button onClick={()=>{
-            console.log(item)
-            // axios.post('/api/book/add',item)
-        }}>add book</button>
-        
-        </div>
-      ))}
-      </div>
+	// set values
 
-    );
-  }
+	const setValues = (item) => {
+		setBookTitle(item.volumeInfo.title);
+		setBookAuthor(item.volumeInfo.authors);
+		setBookCategory(item.volumeInfo.categories);
+		setBookDesc(item.volumeInfo.description);
+		setBookImage(item.volumeInfo.imageLinks.thumbnail);
+	};
 
+	// Make array of objects
+	// Loop through bookArray compare object id with Returned list id
+	// If id's match > pull object from bookArray and pass into axios request
 
-  export default BookForm;
+	const bookArray = []; //UseState
+	const bookValues = (item) => {
+		const book = {};
+		book.push('id', item.volumeInfo.id);
+		book.push('title', item.volumeInfo.title);
+		book.push('author', item.volumeInfo.authors);
+		book.push('category', item.volumeInfo.categories);
+		book.push('desc', item.volumeInfo.description);
+		book.push('image', item.volumeInfo.imageLinks.thumbnail);
+		bookArray.push(book);
+	};
+
+	return (
+		<div>
+			<form
+				className="book-search"
+				onSubmit={(e) => {
+					e.preventDefault();
+					search(title);
+					setTitle('');
+				}}
+			>
+				<label>
+					Book Title:
+					<input
+						type="text"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</label>
+				<input type="submit" value="Search" />
+			</form>
+			{returned.length &&
+				returned.map((item) => (
+					<div key={item.id} className="book-info-api">
+						<h3>{item.volumeInfo.title}</h3>
+						<p>{item.volumeInfo.authors}</p>
+						<p>{item.volumeInfo.categories}</p>
+						<p>{item.volumeInfo.description}</p>
+						<img
+							src={item.volumeInfo.imageLinks.thumbnail}
+							alt="Book Cover"
+						></img>
+						{/* {bookValues(item)} */}
+
+						<button
+							onClick={async (e) => {
+								setValues(item);
+								const bookInfo = {
+									bookTitle,
+									bookAuthor: bookAuthor[0],
+									bookCategory: bookCategory[0],
+									bookDesc,
+									bookImage,
+								};
+								console.log(bookInfo);
+								const resp = await axios.post('/api/books/bookapi', bookInfo);
+								console.log(resp.data);
+							}}
+						>
+							add book
+						</button>
+					</div>
+				))}
+		</div>
+	);
+}
+
+export default BookForm;
