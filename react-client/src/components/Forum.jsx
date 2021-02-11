9c97dd1fd0bedc1f3f4bd1b5e7dbd6ff0027a474
callmekeyz <callmekeyz@gmail.com>
2021-02-11 17:16:01 -0500

    created a forum conpontent

---
+++ react-client/src/components/Forum.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// 
+
+const Forum = () => {
+    return(
+        <h1>Forum</h1>
+    );
+
+};
+
+export default Forum;
---
+++ react-client/src/components/comment/AddComment.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const AddComment = (props) => {
+    const [ comment, setAddComment] = useState("");
+    const processComment = async(e) => {
+        e.preventDefault();
+            comment,
+            id: forumId,
+        };
+        console.log(forumId);
+        let resp = await axios.post("/api/comment" + forumId, comment);
+    }
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default addComment;
