a1dd8b23438311eff6bd7afd09395c27fd896d41
Eunice Pereira <eunicepere333@gmail.com>
2021-02-10 20:53:08 -0600

    added BookForm + BookTable to Member

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBView, MDBMask } from "mdbreact";
-
-
+import {
+	MDBContainer,
+	MDBRow,
+	MDBCol,
+	MDBBtn,
+	MDBView,
+	MDBMask,
+} from 'mdbreact';
 
 const Home = (props) => {
 	return (
@@ -17,37 +22,49 @@ const Home = (props) => {
 				</MDBMask>
 			</MDBView>
 
-			<p>{props.data.status}</p>
 			<MDBContainer>
-				<MDBRow >
+				<MDBRow>
 					<MDBCol md="3">
 						<div class="position-absolute top-0 end-0">
 							<form>
 								<p className="h4 text-center mb-4">Sign in</p>
 								<label htmlFor="defaultFormLoginEmailEx" className="grey-text">
 									Username
-							</label>
-								<input type="email" id="defaultFormLoginEmailEx" className="form-control" />
+								</label>
+								<input
+									type="email"
+									id="defaultFormLoginEmailEx"
+									className="form-control"
+								/>
 								<br />
-								<label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+								<label
+									htmlFor="defaultFormLoginPasswordEx"
+									className="grey-text"
+								>
 									Password
-         					</label>
-								<input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
+								</label>
+								<input
+									type="password"
+									id="defaultFormLoginPasswordEx"
+									className="form-control"
+								/>
 								<div className="text-center mt-4">
-									<MDBBtn color="indigo" type="submit">Login </MDBBtn>
+									<MDBBtn color="indigo" type="submit">
+										Login{' '}
+									</MDBBtn>
 								</div>
 								<div className="text-center mt-4">
-									<MDBBtn color="indigo" type="submit">Create Account</MDBBtn>
+									<MDBBtn color="indigo" type="submit">
+										Create Account
+									</MDBBtn>
 								</div>
 							</form>
 						</div>
 					</MDBCol>
 				</MDBRow>
 			</MDBContainer>
-		</div >
-
+		</div>
 	);
 };
 
 export default Home;
-
